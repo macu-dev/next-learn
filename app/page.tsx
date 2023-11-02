c2cb47ab447ed4881a9a6f197b29f5f0c44557c2
Maria Claudia <mariaclaudiaperezes@gmail.com>
2023-11-02 11:43:15 -0300

    feat(dashboard): add chapter 1 y 2

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,6 @@
 import AcmeLogo from '@/app/ui/acme-logo';
+import styles from '@/app/ui/home.module.css';
+ 
 
 import Link from 'next/link';
 export default function Page() {
@@ -7,6 +9,11 @@ export default function Page() {
       <div className="flex h-20 shrink-0 items-end rounded-lg bg-blue-500 p-4 md:h-52">
         {/* <AcmeLogo /> */}
       </div>
+      <div
+  className="h-0 w-0 border-b-[30px] border-l-[20px] border-r-[20px] border-b-black border-l-transparent border-r-transparent"
+/>
+      <h1 className="text-blue-500">Im blue!</h1>
+      <div className={styles.shape}></div>
       <div className="mt-4 flex grow flex-col gap-4 md:flex-row">
         <div className="flex flex-col justify-center gap-6 rounded-lg bg-gray-50 px-6 py-10 md:w-2/5 md:px-20">
           <p className={`text-xl text-gray-800 md:text-3xl md:leading-normal`}>
